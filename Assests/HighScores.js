8c12474fa51a634f6a25863b569253be22dd624b
Devin Pawline <devinpawline@outlook.com>
2021-05-22 11:41:27 -0400

    Created Highscores HTML and JS, then linked the HTML in index.html as well as using window.replace in JS to go back to the main file after viewing highscores

---
+++ Assests/HighScores.js
@@ -0,0 +1,29 @@
+// Declared variables
+var highScore = document.querySelector("#highScore");
+var clear = document.querySelector("#clear");
+var goBack = document.querySelector("#goBack");
+
+// Event listener to clear scores 
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
+
+// Retreives local stroage and stores it using JSON.parse
+var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
+
+if (allScores !== null) {
+
+    for (var i = 0; i < allScores.length; i++) {
+
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].initials + " " + allScores[i].score;
+        highScore.appendChild(createLi);
+
+    }
+}
+// Event listener to move to main index.html using the file itself 
+goBack.addEventListener("click", function () {
+    window.location.replace("file:///Users/devinpawline/projects/CodingQuiz/index.html");
+});
